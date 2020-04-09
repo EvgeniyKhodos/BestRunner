244f691871524d0e3b2b2e15266f50c326c9dce6
EvgeniyKhodos <en-hodos@mail.ru>
2020-04-09 22:59:58 +0700

    Adds New Fields

--- src/App.js
+++ src/App.js
@@ -13,18 +13,24 @@ function App() {
           <div>
             1
             <a> Дата</a>
+            <input placeholder={"Date"}/>
           </div>
-
           <div>2
             <a> Тип тренировки</a>
+            <input placeholder={"Training type"}/>
           </div>
           <div>3
             <a> Дистанция</a>
+            <input placeholder={"Distance"}/>
           </div>
           <div>3
             <a> Комментарий</a>
+            <input placeholder={"Comment"}/>
           </div>
         </a>
+        <div>
+          <button>Добавить</button>
+        </div>
       </header>
     </div>
   );
